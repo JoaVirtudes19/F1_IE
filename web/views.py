ff7ec6b9634bdde0d8adc16fec9ade28e748b5f9
José Joaquín Virtudes Castro <josejoaquinvirtudes@gmail.com>
2023-03-22 12:08:05 +0100

    Test fastf1

--- web/views.py
+++ web/views.py
@@ -1,10 +1,72 @@
 from django.shortcuts import render
 import matplotlib.pyplot as plt
-import matplotlib
+import matplotlib as mpl
 import io
 import urllib, base64
-matplotlib.use('Agg')
+mpl.use('Agg')
+import fastf1 as ff1
+import numpy as np
+from matplotlib.collections import LineCollection
 # Create your views here.
+
+
+def generarF1():
+    ff1.Cache.enable_cache('./f1cache')  # replace with your cache directory
+    year = 2021
+    wknd = 9
+    ses = 'R'
+    driver = 'RIC'
+    colormap = mpl.cm.plasma
+    session = ff1.get_session(year, wknd, ses)
+    weekend = session.event
+    session.load()
+    lap = session.laps.pick_driver(driver).pick_fastest()
+
+    # Get telemetry data
+    x = lap.telemetry['X']              # values for x-axis
+    y = lap.telemetry['Y']              # values for y-axis
+    color = lap.telemetry['Speed']     
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    # We create a plot with title and adjust some setting to make it look good.
+    fig, ax = plt.subplots(sharex=True, sharey=True, figsize=(12, 6.75))
+    fig.suptitle(f'{weekend.name} {year} - {driver} - Speed', size=24, y=0.97)
+
+    # Adjust margins and turn of axis
+    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.12)
+    ax.axis('off')
+
+
+    # After this, we plot the data itself.
+    # Create background track line
+    ax.plot(lap.telemetry['X'], lap.telemetry['Y'], color='black', linestyle='-', linewidth=16, zorder=0)
+
+    # Create a continuous norm to map from data points to colors
+    norm = plt.Normalize(color.min(), color.max())
+    lc = LineCollection(segments, cmap=colormap, norm=norm, linestyle='-', linewidth=5)
+
+    # Set the values used for colormapping
+    lc.set_array(color)
+
+    # Merge all line segments together
+    line = ax.add_collection(lc)
+
+
+    # Finally, we create a color bar as a legend.
+    cbaxes = fig.add_axes([0.25, 0.05, 0.5, 0.05])
+    normlegend = mpl.colors.Normalize(vmin=color.min(), vmax=color.max())
+    legend = mpl.colorbar.ColorbarBase(cbaxes, norm=normlegend, cmap=colormap, orientation="horizontal")
+
+
+    # Show the plot
+    buf = io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    plt.close("all")
+
+    return uri
 
 def generar(n,title):
     plt.plot(range(n))
@@ -15,10 +77,12 @@ def generar(n,title):
     buf.seek(0)
     string = base64.b64encode(buf.read())
     uri = urllib.parse.quote(string)
+    plt.close('all')
+
     return uri
 
 def inicio(request):
-    graficas = [generar(10,"primera"),generar(20,"segunda")]
+    graficas = [generar(10,"primera"),generarF1(),generar(20,"segunda")]
     return render(request,'inicio.html',{'charts':graficas})
 
 
